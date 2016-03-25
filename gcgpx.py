3406821d73737006a55b91d3a1aec36c8478d90b
edstott <ed.stott@imperial.ac.uk>
2016-03-25 08:54:17 +0000

    Update gcgpx.py
    Changed prints to functions

--- gcgpx.py
+++ gcgpx.py
@@ -28,39 +28,39 @@ else:
 	path = defaultpath
 
 searchpath = os.path.join(path,filemask)	
-print 'Searching '+searchpath
+print('Searching '+searchpath)
 
 files = [filename for filename in glob.glob(searchpath) if time.time() - os.stat(filename).st_mtime < gpxage]
 
-print 'Found '+str(len(files))+' gpx files'
+print('Found '+str(len(files))+' gpx files')
 
 for filename in files:
 	
-	print 'Processing '+filename
+	print('Processing '+filename)
 	gpxdom = minidom.parse(filename)
 	
 	try:
 		geocode = gpxdom.getElementsByTagName('wpt')[0].getElementsByTagName('name')[0].firstChild.data
 	except AttributeError:
-		print 'Did not find reference code in '+filename
+		print('Did not find reference code in '+filename)
 		continue
 		
 	try:
 		sdescnode = gpxdom.getElementsByTagName('groundspeak:short_description')[0].firstChild
 	except:
-		print 'Did not find short description node in  '+filename
+		print('Did not find short description node in  '+filename)
 		sdescnode = None
 		
 	try:
 		ldescnode = gpxdom.getElementsByTagName('groundspeak:long_description')[0].firstChild
 	except:
-		print 'Did not find long description node in  '+filename
+		print('Did not find long description node in  '+filename)
 		ldescnode = None
 		
 	try:
 		hintnode = gpxdom.getElementsByTagName('groundspeak:encoded_hints')[0].firstChild
 	except:
-		print 'Did not find hint node in  '+filename
+		print('Did not find hint node in  '+filename)
 		hintnode = None
 	
 	geopage = requests.get('http://www.geocaching.com/geocache/'+geocode)
@@ -68,7 +68,7 @@ for filename in files:
 
 	hintobj = geohtml.find("div",id="div_hint")
 	if hintobj is None:
-		print 'Did not find hint for '+geocode
+		print('Did not find hint for '+geocode)
 		hint = ""
 	elif hintnode is not None:
 		hint = re.search("\s*(.*)",hintobj.text).group(1)
@@ -77,14 +77,14 @@ for filename in files:
 		
 	shortdescobj = geohtml.find("span",id="ctl00_ContentBody_ShortDescription")
 	if shortdescobj is None:
-		print 'Did not find short description for '+geocode
+		print('Did not find short description for '+geocode)
 		shortdesc = ""
 	elif sdescnode is not None:
 		sdescnode.data = shortdescobj.text
 		
 	longdescobj = geohtml.find("span",id="ctl00_ContentBody_LongDescription")
 	if longdescobj is None:
-		print 'Did not find long description for '+geocode
+		print('Did not find long description for '+geocode)
 		longdesc = ""
 	elif ldescnode is not None:
 		ldescnode.data = longdescobj.text
